39c0aeeb678b7de59edd268fcea8f163d5b20486
Chris Gkalfas <chris_skart@hotmail.com>
2017-02-04 10:53:27 +0200

    TourGuideProject
    First code.

---
+++ app/src/main/java/com/example/android/tourguideapp/ThingsToDo.java
@@ -0,0 +1,10 @@
+package com.example.android.tourguideapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chris_skart on 30/01/2017.
+ */
+
+public class ThingsToDo extends AppCompatActivity {
+}
